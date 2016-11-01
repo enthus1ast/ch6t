02e12be7f75b194d609162ee9d11fbd717fd5291
ent <ent@example.com>
2016-11-01 13:23:55 +0100

    file system links '\\' werden von den browsers leider blockert, ist daher ein bisschen verfickt. Jedoch werden jetzt die links in einem neuen tab geöffnet

--- static/scripts/external/autolink.js
+++ static/scripts/external/autolink.js
@@ -6,9 +6,9 @@
   autoLink = function() {
     var callback, k, linkAttributes, option, options, pattern, v;
     options = 1 <= arguments.length ? slice.call(arguments, 0) : [];
-    pattern = /(^|[\s\n]|<[A-Za-z]*\/?>)(((?:https?|ftp):\/\/|(?:\\\\?))[\-A-Z0-9+\u0026\u2019@#\/%?=()~_|!:,.;]*[\-A-Z0-9+\u0026@#\/%=~()_|])/gi;
+    pattern = /(^|[\s\n]|<[A-Za-z]*\/?>)((?:https?|ftp):\/\/[\-A-Z0-9+\u0026\u2019@#\/%?=()~_|!:,.;]*[\-A-Z0-9+\u0026@#\/%=~()_|])/gi;
     if (!(options.length > 0)) {
-      return this.replace(pattern, "$1<a href='$2'>$2</a>");
+      return this.replace(pattern, "$1<a target='_blank' href='$2'>$2</a>");
     }
     option = options[0];
     callback = option["callback"];
